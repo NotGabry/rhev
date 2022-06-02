d1ef8ea5b5b7e09a062ff583f7a49d54b35657fb
Gabry-Pro <gabryuso88@gmail.com>
2022-06-02 17:08:56 +0200

    Adding Source

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 350,
+    height: 530,
+    webPreferences: {
+      devTools: false,
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    frame: false,
+    resizable: false
+  })
+
+  mainWindow.loadFile(path.join(__dirname, 'App', 'index.html'))
+}
+
+// APP
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
+
+// IPC
+ipcMain.on('close-window', async () => {
+  await app.quit()
+})
+ipcMain.on('min-window', async () => {
+  await BrowserWindow.getFocusedWindow().minimize()
+})
